ad30f31f5c8b5edd07e64fbb10c2717b34acff0a
melvyn2 <melvyn2@brcok.tk>
2023-03-08 18:06:59 -0800

    Move runtime scripts to game_clean

---
+++ game_clean/copy/run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")"
+
+MANGO="$(command -v mangohud || true)"
+ARGS="-steam -game tf -insecure -novid -nojoy -nosteamcontroller -nohltv -particles 1 -noborder -particle_fallback 2 -dev -nobreakpad -console"
+export LD_LIBRARY_PATH="${PWD}/bin:${PWD}/tf/bin"
+
+if [[ "$1" == "-d" ]]; then
+	# shellcheck disable=SC2086
+	# shellcheck disable=SC2068
+	lldb ./hl2_linux -- -allowdebug ${ARGS} ${@:2}
+elif [[ "$1" == "-b" ]]; then
+	rm tf/cfg/config.cfg
+	mkdir -p ../benchlogs
+	# shellcheck disable=SC2086
+	MANGOHUD_DLSYM=1 MANGOHUD_CONFIG="output_folder=${PWD}/../benchlogs,toggle_logging=F12" $MANGO \
+		"${PWD}"/hl2_linux ${ARGS} "+bind f12 demo_resume" "+fps_max 0" "+playdemo demos/${2}" "+demo_pause"
+elif [[ "$1" == "-s" ]]; then
+	# shellcheck disable=SC2068
+	"${PWD}"/srcds_linux ${ARGS} $@
+else
+	# shellcheck disable=SC2086
+	MANGOHUD_DLSYM=1 ${MANGO} "${PWD}"/hl2_linux ${ARGS} $@
+fi
